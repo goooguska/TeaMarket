5b4dd17aeb473a75c6b07de539181516d5de71dc
Sasha <cahalego@mail.ru>
2023-10-04 21:07:49 +0300

    feat:add new changes

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,23 @@
-function funcDropDown() {
-	document.getElementById('dropDown').classList.toggle('show');
-}
+const dropdowns = document.querySelectorAll('.header-bottom__dropdown-menu');
+dropdowns.forEach(dropdown => {
+	const select = dropdown.querySelector('.header-bottom__dropdown-menu__category-name')
+	const arrow = dropdown.querySelector('.arrow');
+	const menu = dropdown.querySelector('.header-bottom__dropdown-menu__content');
+	const options = dropdown.querySelectorAll('.header-bottom__dropdown-menu__content li');
+	select.addEventListener('click', () =>{
+		arrow.classList.toggle('arrow-rotate');
+		menu.classList.toggle('header-bottom__dropdown-menu__content-open')
+		console.log("aaa");
+	});
+	options.forEach(option =>{
+		option.addEventListener('click',() => {
+			arrow.classList.remove('arrow-rotate');
+			menu.classList.remove('header-bottom__dropdown-menu__content-open')
+			
+		})
+	})
+
+
+
+
+})

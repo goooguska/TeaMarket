3a6cc9d20a760b6fb77544ea1add4b873e8f30fc
Sasha <cahalego@mail.ru>
2023-10-04 22:05:45 +0300

    feat:add new changes

--- src/js/index.js
+++ src/js/index.js
@@ -1,23 +1,38 @@
-const dropdowns = document.querySelectorAll('.header-bottom__dropdown-menu');
-dropdowns.forEach(dropdown => {
-	const select = dropdown.querySelector('.header-bottom__dropdown-menu__category-name')
-	const arrow = dropdown.querySelector('.arrow');
-	const menu = dropdown.querySelector('.header-bottom__dropdown-menu__content');
-	const options = dropdown.querySelectorAll('.header-bottom__dropdown-menu__content li');
-	select.addEventListener('click', () =>{
-		arrow.classList.toggle('arrow-rotate');
-		menu.classList.toggle('header-bottom__dropdown-menu__content-open')
-		console.log("aaa");
-	});
-	options.forEach(option =>{
-		option.addEventListener('click',() => {
-			arrow.classList.remove('arrow-rotate');
-			menu.classList.remove('header-bottom__dropdown-menu__content-open')
-			
+
+	const dropdowns = document.querySelectorAll('.header-bottom__dropdown-menu');
+	dropdowns.forEach(dropdown => {
+		const select = document.querySelector('.header-bottom__dropdown-menu__category-name')
+		const arrow = dropdown.querySelector('.arrow');
+		const menu = dropdown.querySelector('.header-bottom__dropdown-menu__content');
+		const options = dropdown.querySelectorAll('.header-bottom__dropdown-menu__content li');
+		select.addEventListener('click', () =>{
+			arrow.classList.toggle('arrow-rotate');
+			menu.classList.toggle('header-bottom__dropdown-menu__content-open')
+			console.log("aaa");
+		});
+		options.forEach(option =>{
+			option.addEventListener('click',() => {
+				arrow.classList.remove('arrow-rotate');
+				menu.classList.remove('header-bottom__dropdown-menu__content-open');
+				option.classList.remove('header-bottom__dropdown-menu__category-name')
+				options.forEach(option =>{
+					option.classList.remove('header-bottom__dropdown-menu__category-name')
+				})
+				option.classList.add('header-bottom__dropdown-menu__category-name')
+			})
 		})
+	
+	
+	
+	
 	})
 
 
 
 
-})
+// function dropDown(){
+// 	let selectedOption = select.options[select.selectedIndex].value;
+  
+//   console.log("Выбрана опция: " + selectedOption);
+// }options.forEach(option =>{
+
